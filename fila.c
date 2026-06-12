779834fa0d3e8abe1ffcf85bb6836a56c5ad02e6
Rafael Oliveira <rafaf1604@gmail.com>
2026-06-12 01:30:32 -0300

    TAD Livro e TAD Fila

--- fila.c
+++ fila.c
@@ -1,1 +1,92 @@
 #include "fila.h"
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Fila* criarFila() {
+    Fila* fila;
+    fila->inicio = NULL;
+    fila->fim = NULL;
+    return fila;
+}
+
+int filaVazia(Fila* fila) {
+    if (fila == NULL || fila->inicio == NULL) {
+        return 1; // vazia
+    }
+    return 0; // não vazia
+}
+
+// função auxiliar de criação de nós
+NoFila* criarNoFila() {
+    NoFila* novoNo = (NoFila*) malloc(sizeof(NoFila));
+    if (novoNo == NULL) {
+        printf("Erro de alocação de memória na fila.\n");
+        return NULL;
+    }
+    return novoNo;
+}
+
+void enfileirarReserva(Fila* fila, Reserva reserva) {
+    if (fila == NULL) return;
+
+    NoFila* novoNo = criarNoFila();
+    if (novoNo == NULL){ return; }
+
+    // Copia os dados da reserva de forma segura
+    novoNo->reserva.codigoLivro = reserva.codigoLivro;
+    strncpy(novoNo->reserva.nomeUsuario, reserva.nomeUsuario, 99);
+    novoNo->reserva.nomeUsuario[99] = '\0';
+    novoNo->prox = NULL;
+
+    // Lógica FIFO (First In, First Out)
+    if (filaVazia(fila)) {
+        fila->inicio = novoNo;
+        fila->fim = novoNo;
+    } else {
+        fila->fim->prox = novoNo;
+        fila->fim = novoNo;
+    }
+}
+
+Reserva desenfileirarReserva(Fila* fila) {
+    Reserva reservaVazia = {"", -1};
+
+    if (fila == NULL || filaVazia(fila)) {
+        printf("Fila de reservas está vazia.\n");
+        return reservaVazia;
+    }
+
+    NoFila* temp = fila->inicio;
+    
+    Reserva reservaRecuperada = temp->reserva;
+
+    fila->inicio = fila->inicio->prox;
+
+    if (fila->inicio == NULL) {
+        fila->fim = NULL;
+    }
+
+    free(temp);
+
+    return reservaRecuperada;
+}
+
+void exibirReservas(Fila* fila) {
+    if (fila->inicio == NULL) {
+        printf("Nenhuma reserva registrada na fila global.\n");
+        return;
+    }
+
+    NoFila* atual = fila->inicio;
+    printf("\n--- FILA GLOBAL DE RESERVAS ---\n");
+    int pos = 1;
+    while (atual != NULL) {
+        printf("%d. Usuário: %s | Código do Livro: %d\n",
+               pos++, atual->reserva.nomeUsuario, atual->reserva.codigoLivro);
+        atual = atual->prox;
+    }
+    printf("-------------------------------\n");
+}
--- livro.c
+++ livro.c
@@ -1,13 +1,77 @@
 #include "livro.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Livro* criarLivro(int codigo, char titulo[], char autor[], int ano, int quantidadeTotal) {
+    Livro* novoLivro = malloc(sizeof(Livro));
+
+    if (novoLivro == NULL) {
+        printf("Erro crônico: Falha ao alocar memória para o livro.\n");
+        return NULL;
+    }
+
+    novoLivro->codigo = codigo;
+    novoLivro->ano = ano;
+    novoLivro->quantidadeTotal = quantidadeTotal;
+    novoLivro->quantidadeDisponivel = quantidadeTotal; // inicialmente são iguais
+
+    strncpy(novoLivro->titulo, titulo, 99);
+    novoLivro->titulo[99] = '\0';
+
+    strncpy(novoLivro->autor, autor, 99);
+    novoLivro->autor[99] = '\0';
+
+    return novoLivro;
+}
 
 void exibirLivro(Livro* livro) {
-    printf("[ID: %04d] %-30s | Autor: %-20s | Ano: %d | Total: %d | Disp: %d\n",
-           livro->codigo,
-           livro->titulo,
-           livro->autor,
-           livro->ano,
-           livro->quantidadeTotal,
-           livro->quantidadeDisponivel);
+    if (livro == NULL) {
+        printf("Erro: Ponteiro de livro nulo.\n");
+        return;
+    }
+    printf("========================================\n");
+    printf("Código: %d\n", livro->codigo);
+    printf("Título: %s\n", livro->titulo);
+    printf("Autor:  %s\n", livro->autor);
+    printf("Ano:    %d\n", livro->ano);
+    printf("Total:  %d | Disponível: %d\n", livro->quantidadeTotal, livro->quantidadeDisponivel);
+    printf("========================================\n");
+}
+
+int obterCodigoLivro(Livro* livro) {
+    if (livro != NULL) {
+        return livro->codigo;
+    }
+    return -1; // código de erro caso o ponteiro seja nulo
+}
+
+int obterQuantidadeDisponivel(Livro* livro) {
+    if (livro != NULL) {
+        return livro->quantidadeDisponivel;
+    }
+    return -1;
+}
+
+void emprestarExemplar(Livro* livro) {
+    if (livro != NULL && livro->quantidadeDisponivel > 0) {
+        livro->quantidadeDisponivel--;
+        return;
+    }
+    printf("Não foi possível emprestar o exemplar!");
+}
+
+void devolverExemplar(Livro* livro) {
+    if (livro != NULL && livro->quantidadeDisponivel < livro->quantidadeTotal) {
+        livro->quantidadeDisponivel++;
+        return;
+    }
+    printf("Não foi possível devolver o exemplar!");
+}
+
+void liberarLivro(Livro* livro) {
+    if (livro != NULL) {
+        free(livro); 
+    }
 }
